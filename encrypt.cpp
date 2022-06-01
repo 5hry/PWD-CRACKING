5a1982df4805c95869ca4c3beb0e6e44e15860f9
5hry <96819819+5hry@users.noreply.github.com>
2022-06-01 21:16:05 +0700

    Create encrypt.cpp

---
+++ encrypt.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+#include "key.hpp"
+
+std::string password;
+
+
+int main()
+{
+    int i;
+    std::string buffer;
+    std::vector<Key> T(N); // the table T
+    Key ssum;
+
+    std::cin >> password;
+    Key passkey((std::string(password)));
+
+    std::fstream input("input.txt");
+    for (i = 0; i < N; i++)
+    {
+        std::getline(input, buffer);
+        T[i].set_string(buffer);
+    }
+    input.close();
+
+    ssum = passkey.subset_sum(T);
+    ssum.show();
+
+    return 0;
+}
